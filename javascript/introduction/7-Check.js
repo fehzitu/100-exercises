47c1f836683acddfbc36d959d9c92e7a5ea3cb80
fehzitu <fehzitujsdev@gmail.com>
2026-03-25 06:42:50 -0300

    ex 7 in js

---
+++ javascript/introduction/7-Check.js
@@ -0,0 +1,10 @@
+// 7. Check if a number is even or odd.
+
+let val1 = 10;
+
+// if the remainder of any value divided by 2 is 0, then it is even.
+if (val1 % 2 == 0) {
+    console.log('even');
+} else {
+    console.log('odd');
+};
