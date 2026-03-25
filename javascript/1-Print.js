8a125b60ce386a61e1933f25722e4a7c6df890c4
fehzitu <fehzitujsdev@gmail.com>
2026-03-25 05:30:42 -0300

    ex 2 in js

--- javascript/1-Print.js
+++ javascript/1-Print.js
@@ -1,2 +1,3 @@
 // 1- Print "Hello, World!" to the console.
+
 console.log('Hello, World!');
---
+++ javascript/2-Declare.js
@@ -0,0 +1,5 @@
+// 2- Declare variables of different types (string, number, boolean).
+
+let str = 'This is a string';
+let num = 10;
+let bool = true;
