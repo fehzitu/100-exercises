4f0f0177363a90bf6e72abcbc465a4ac751be8e6
fehzitu <fehzitujsdev@gmail.com>
2026-03-26 13:42:12 -0300

    ex 9 js

---
+++ javascript/introduction/9-Find2.js
@@ -0,0 +1,11 @@
+// 8. Find the largest of three numbers.
+
+let num1 = 100;
+let num2 = 50;
+let num3 = 250;
+
+// Returns the larger number
+let res = Math.max(num1, num2, num3);
+
+// show res
+console.log(res);
