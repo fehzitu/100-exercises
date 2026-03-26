6d523b546018a38678342e0244006bce64fbc4d4
fehzitu <fehzitujsdev@gmail.com>
2026-03-26 13:45:24 -0300

    minor chances

---
+++ javascript/introduction/10-Calculate.js
@@ -0,0 +1,11 @@
+// 10. Calculate the average of 3 numbers.
+
+let num1 = 100;
+let num2 = 200;
+let num3 = 500;
+
+// Here, the values ​​were added together and divided by 3 (the total number of values).
+let res = (num1 + num2 + num3) / 3;
+
+// show res
+console.log(res);
