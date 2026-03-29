be4302913eaaa03342b31667f09489b50590bc1f
fehzitu <fehzitujsdev@gmail.com>
2026-03-29 05:50:03 -0300

    ex 12 js

---
+++ javascript/introduction/12-Check.js
@@ -0,0 +1,12 @@
+// 12. Check if a number is positive, negative, or zero.
+
+let num1 = -10;
+
+// check and print
+if (num1 > 0) {
+    console.log(num1, 'is positive');
+} else if (num1 < 0) {
+    console.log(num1, 'is negative');
+} else {
+    console.log(num1, 'is zero');
+};
