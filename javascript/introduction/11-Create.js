baa405d423b5334673320dc367cc4999e25c097c
fehzitu <fehzitujsdev@gmail.com>
2026-03-29 05:44:44 -0300

    ex 11 js

---
+++ javascript/introduction/11-Create.js
@@ -0,0 +1,12 @@
+// 11. Create a simple calculator (2 numbers + operator).
+
+let num1 = 10;
+let num2 = 20;
+
+// calcs
+let res1 = num1 + num2;
+let res2 = num1 * num2;
+let res3 = num1 / num2;
+
+// show res
+console.log(res1, res2, res3);
