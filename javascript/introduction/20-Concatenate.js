1baa035b0591a187c9af17fe884694e82d8849d1
fehzitu <fehzitujsdev@gmail.com>
2026-03-31 06:37:44 -0300

    ex 20 js

---
+++ javascript/introduction/20-Concatenate.js
@@ -0,0 +1,8 @@
+// 20. Concatenate two strings.
+
+let str1 = 'Hello';
+let str2 = 'World!';
+
+// show res
+console.log(`${str1}, ${str2}`);
+// When we use `` (craze) we can then concatenate variables within strings, just pass the variable within a variable block "${variable_here}"
