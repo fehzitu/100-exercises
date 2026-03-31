fe593c29542d3206a440a18df96bdb2457779580
fehzitu <fehzitujsdev@gmail.com>
2026-03-31 06:32:31 -0300

    ex 19 js

---
+++ javascript/introduction/19-Check.js
@@ -0,0 +1,8 @@
+// 19. Check if a string is empty.
+
+let empStr = '';
+let string = 'Hello, world';
+
+// show res
+console.log(empStr.length, string.length);
+// If an string.length show 0, the string is empty
